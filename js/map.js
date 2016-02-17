dd240b82c8fd57081d38ed423654531bc0dc4526
votandat061 <votandat061@gmail.com>
2016-02-16 16:08:02 -0800

    fixed major bug

--- js/map.js
+++ js/map.js
@@ -1,23 +1,46 @@
 var map;
+var myLatLng;
 
 function alertMax(alertString) {
 	Android.alert(alertString);
 }
-
+//function test(){
+//var center = new google.maps.LatLng(47.24573265, -122.4372974, 18);
+//map.setCenter(center);
+//map.setZoom(zoom: 19);
+//}
 function centerCamera(targetName, targetLat, targetLng, zoomLevel) {
-	var center = new google.maps.LatLng(targetLat, targetLng);
-	map.setCenter(center);
-//map.setZoom(zoomLevel);
-//  var marker = new google.maps.Marker({
-//    position: {lat: targetLat, lng: targetLng},
-//    map: map,
-//    title: targetName
-//  });
-//  marker.setMap(map);
+var center = new google.maps.LatLng(targetLat, targetLng);
+map.setCenter(center);
+map.setZoom(zoomLevel);
+  var marker = new google.maps.Marker({
+    position: {lat: targetLat, lng: targetLng},
+    map: map,
+    title: targetName
+  });
+
+
+  var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">Description</h1>'+
+      '<div id="bodyContent">'+
+      '<p>this is the description for .</p>'+ targetName +
+      '<img src="file:///android_asset/common/bb107.png" alt=targetName height="128" width="128"</img>'+
+      '</div>'+
+      '</div>';
+
+  var infoWindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+    marker.addListener('click', function() {
+      infoWindow.open(map, marker);
+    });
+  marker.setMap(map);
+
 
 //add listener to marker
 }
-
 function initMap() {
 	var mapElement = document.getElementById('map');
 
@@ -43,4 +66,5 @@ function initMap() {
 
 	map = new google.maps.Map(mapElement, mapOptions);
 	map.setOptions({ styles: styles });
+	myLatLng = new google.maps.LatLng;
 }
